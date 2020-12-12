6c1829fdce6ad9663b6672e102c513789ab134b4
rifandani <ipandani2505@gmail.com>
2020-12-12 17:26:45 +0700

    :hammer: REFACTOR: next config ignore build error

--- next.config.js
+++ next.config.js
@@ -7,6 +7,6 @@ module.exports = {
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     // !! WARN !!
-    ignoreBuildErrors: false,
+    ignoreBuildErrors: true,
   },
 };
