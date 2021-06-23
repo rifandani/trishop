551cd9391b859d3dfb0047112ffba7512d19c7d1
rifandani <ipandani2505@gmail.com>
2021-06-23 15:33:00 +0700

    chore(global): delete ccc folder

--- ccc/support/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { TRegisterApiSchema, TLoginApiSchema } from '../../src/yup/apiSchema'
-
-declare namespace Cypress {
-  interface Chainable {
-    // auth
-    loginUser(requestBody: TLoginApiSchema): Chainable<Response>
-    registerUser(requestBody: TRegisterApiSchema): Chainable<Response>
-  }
-}
